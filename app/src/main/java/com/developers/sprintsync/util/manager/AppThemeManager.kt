a9a79396a999d36dd55b6b28d39ea27be20e37bb
Roman <roman.gallyamov@gmail.com>
2024-01-18 15:43:56 +0500

    Themes, AppThemeManager update

--- app/src/main/java/com/developers/sprintsync/util/manager/AppThemeManager.kt
+++ app/src/main/java/com/developers/sprintsync/util/manager/AppThemeManager.kt
@@ -2,9 +2,18 @@ package com.developers.sprintsync.util.manager
 
 import android.content.Context
 import android.util.TypedValue
+import com.developers.sprintsync.R
 
 
 class AppThemeManager(private val context: Context) {
+
+    fun getPrimaryColor(): Int {
+        val typedValue = TypedValue()
+        context.theme.resolveAttribute(
+            com.google.android.material.R.attr.colorPrimary, typedValue, true
+        )
+        return typedValue.data
+    }
 
     fun getSecondaryColor(): Int {
         val typedValue = TypedValue()
@@ -14,10 +23,18 @@ class AppThemeManager(private val context: Context) {
         return typedValue.data
     }
 
-    fun getPrimaryColor(): Int {
+    fun getThirdlyColor(): Int {
         val typedValue = TypedValue()
         context.theme.resolveAttribute(
-            com.google.android.material.R.attr.colorPrimary, typedValue, true
+            R.attr.colorThirdly, typedValue, true
+        )
+        return typedValue.data
+    }
+
+    fun getFourthlyColor(): Int {
+        val typedValue = TypedValue()
+        context.theme.resolveAttribute(
+            R.attr.colorFourthly, typedValue, true
         )
         return typedValue.data
     }
