2d4fce7ba1c679cfa2af7372e50bbca8afaf8128
Roman <roman.gallyamov@gmail.com>
2024-01-25 16:19:02 +0500

    LocationManagerModule for Hilt init, Location Play Services in Gradle added

--- app/src/main/java/com/developers/sprintsync/di/Modules.kt
+++ app/src/main/java/com/developers/sprintsync/di/Modules.kt
@@ -1,2 +1,17 @@
 package com.developers.sprintsync.di
 
+import com.developers.sprintsync.manager.location.LocationManager
+import com.developers.sprintsync.manager.location.LocationManagerImpl
+import dagger.Binds
+import dagger.Module
+import dagger.hilt.InstallIn
+import dagger.hilt.components.SingletonComponent
+import javax.inject.Singleton
+
+@Module
+@InstallIn(SingletonComponent::class)
+abstract class LocationManagerModule() {
+    @Binds
+    @Singleton
+    abstract fun bindLocationManager(impl: LocationManagerImpl): LocationManager
+}
