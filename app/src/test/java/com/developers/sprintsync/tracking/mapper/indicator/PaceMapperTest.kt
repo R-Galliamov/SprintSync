4d856a76240476d4d1e1f7627c5e5a4139bb1289
Roman <roman.gallyamov@gmail.com>
2024-03-12 10:09:49 +0500

    Add PaceMapperTest

---
+++ app/src/test/java/com/developers/sprintsync/tracking/mapper/indicator/PaceMapperTest.kt
@@ -0,0 +1,66 @@
+package com.developers.sprintsync.tracking.mapper.indicator
+
+import org.junit.Assert.assertEquals
+import org.junit.Test
+import org.junit.runner.RunWith
+import org.junit.runners.JUnit4
+import java.util.Locale
+
+@RunWith(JUnit4::class)
+class PaceMapperTest {
+    @Test
+    fun paceToPresentablePace_returnsCorrectlyFormattedRoundedUpPace() {
+        // Arrange
+        val pace = 1.235f
+        val expected = "1.24"
+        Locale.setDefault(Locale.US)
+
+        // Act
+        val result = PaceMapper.paceToPresentablePace(pace)
+
+        // Assert
+        assertEquals(expected, result)
+    }
+
+    @Test
+    fun paceToPresentablePace_returnsCorrectlyFormattedRoundedDownPace() {
+        // Arrange
+        val pace = 1.234f
+        val expected = "1.23"
+        Locale.setDefault(Locale.US)
+
+        // Act
+        val result = PaceMapper.paceToPresentablePace(pace)
+
+        // Assert
+        assertEquals(expected, result)
+    }
+
+    @Test
+    fun paceToPresentablePace_returnsCorrectlyFormattedRoundedUpPaceWithDifferentLocale() {
+        // Arrange
+        val pace = 1.235f
+        val expected = "1,24"
+        Locale.setDefault(Locale.GERMANY)
+
+        // Act
+        val result = PaceMapper.paceToPresentablePace(pace)
+
+        // Assert
+        assertEquals(expected, result)
+    }
+
+    @Test
+    fun paceToPresentablePace_returnsCorrectlyFormattedRoundedDownPaceWithDifferentLocale() {
+        // Arrange
+        val pace = 1.234f
+        val expected = "1,23"
+        Locale.setDefault(Locale.GERMANY)
+
+        // Act
+        val result = PaceMapper.paceToPresentablePace(pace)
+
+        // Assert
+        assertEquals(expected, result)
+    }
+}
