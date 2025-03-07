a793eb3eb493fb6070f78d67e7bd7456096a571d
roman <roman.gallyamov@gmail.com>
2025-03-07 11:32:23 +0100

    Add tests and comments on polyline utils

--- app/src/main/java/com/developers/sprintsync/tracking_session/presentation/tracking/util/polyline/PolylineOptionsCreator.kt
+++ app/src/main/java/com/developers/sprintsync/tracking_session/presentation/tracking/util/polyline/PolylineOptionsCreator.kt
@@ -5,16 +5,23 @@ import com.google.android.gms.maps.model.LatLng
 import com.google.android.gms.maps.model.PolylineOptions
 import javax.inject.Inject
 
+/** Creates PolylineOptions for map rendering with consistent styling. */
 class PolylineOptionsCreator
-    @Inject
-    constructor() {
-        companion object {
-            private const val POLYLINE_COLOR = Color.RED // TODO fix color providing
-            private const val POLYLINE_WIDTH = 7.5f
-        }
-
-        fun create(polylines: List<List<LatLng>>): List<PolylineOptions> =
-            polylines.map { segment ->
-                PolylineOptions().color(POLYLINE_COLOR).width(POLYLINE_WIDTH).addAll(segment)
-            }
+@Inject
+constructor() {
+    companion object {
+        private const val POLYLINE_COLOR = Color.RED
+        private const val POLYLINE_WIDTH = 7.5f
     }
+
+    /**
+     * Converts a list of polylines into styled PolylineOptions using predefined color and line width.
+     *
+     * @param polylines List of polylines, each a list of LatLng points.
+     * @return List of PolylineOptions with default color and width.
+     */
+    fun create(polylines: List<List<LatLng>>): List<PolylineOptions> =
+        polylines.map { segment ->
+            PolylineOptions().color(POLYLINE_COLOR).width(POLYLINE_WIDTH).addAll(segment)
+        }
+}
--- app/src/main/java/com/developers/sprintsync/tracking_session/presentation/tracking/util/polyline/PolylineProcessor.kt
+++ app/src/main/java/com/developers/sprintsync/tracking_session/presentation/tracking/util/polyline/PolylineProcessor.kt
@@ -4,15 +4,24 @@ import com.developers.sprintsync.core.components.track.data.model.Segments
 import com.google.android.gms.maps.model.PolylineOptions
 import javax.inject.Inject
 
+/**
+ * Generates PolylineOptions for map rendering by formatting segments into polylines and styling them.
+ * Uses injected PolylineFormatter and PolylineOptionsCreator.
+ */
 class PolylineProcessor
     @Inject
     constructor(
         private val polylineOptionsCreator: PolylineOptionsCreator,
         private val polylineFormatter: PolylineFormatter,
     ) {
-        fun generateNewPolylines(segments: Segments): List<PolylineOptions> {
+        /**
+         * Converts segments into a list of styled PolylineOptions for map rendering.
+         *
+         * @param segments The list of segments to process.
+         * @return A list of PolylineOptions.
+         */
+        fun generatePolylines(segments: Segments): List<PolylineOptions> {
             val polylines = polylineFormatter.format(segments)
-            val polylineOptions = polylineOptionsCreator.create(polylines)
-            return polylineOptions
+            return polylineOptionsCreator.create(polylines)
         }
     }
