0f3b65078250f95f440aae1d354e0ff9fb48b9f1
roman <roman.gallyamov@gmail.com>
2025-04-22 16:56:11 +0200

    refactor: migrate build to version catalogs

--- app/src/main/java/com/developers/sprintsync/data/track/repository/TrackRepositoryImpl.kt
+++ app/src/main/java/com/developers/sprintsync/data/track/repository/TrackRepositoryImpl.kt
@@ -11,40 +11,40 @@ import javax.inject.Singleton
 
 @Singleton
 class TrackRepositoryImpl
-    @Inject
-    constructor(
-        private val trackDao: TrackDao,
-    ) : TrackRepository {
-        private val cache = LruCache<Int, Track>(CACHE_SIZE)
-        private val mutex = Mutex()
+@Inject
+constructor(
+    private val trackDao: TrackDao,
+) : TrackRepository {
+    private val cache = LruCache<Int, Track>(CACHE_SIZE)
+    private val mutex = Mutex()
 
-        private var lastTrackId: Int? = null
+    private var lastTrackId: Int? = null
 
-        override val tracks = trackDao.getAllTracks()
+    override val tracks = trackDao.getAllTracks()
 
-        override suspend fun saveTrack(track: Track): Int {
-            val entity = TrackEntity.fromDto(track)
-            val trackId = trackDao.insertTrack(entity).toInt()
-            mutex.withLock { cache.put(trackId, track) }
-            lastTrackId = trackId
-            return trackId
-        }
-
-        override suspend fun getLastTrack(): Track? = lastTrackId?.let { getTrackById(it) }
-
-        override suspend fun getTrackById(id: Int): Track =
-            mutex.withLock {
-                cache[id] ?: trackDao.getTrackById(id).also {
-                    cache.put(id, it)
-                }
-            }
-
-        override suspend fun deleteTrackById(id: Int) {
-            trackDao.deleteTrackById(id)
-            mutex.withLock { cache.remove(id) }
-        }
-
-        companion object {
-            private const val CACHE_SIZE = 1
-        }
+    override suspend fun saveTrack(track: Track): Int {
+        val entity = TrackEntity.fromDto(track)
+        val trackId = trackDao.insertTrack(entity).toInt()
+        mutex.withLock { cache.put(trackId, track) }
+        lastTrackId = trackId
+        return trackId
     }
+
+    override suspend fun getLastTrack(): Track? = lastTrackId?.let { getTrackById(it) }
+
+    override suspend fun getTrackById(id: Int): Track =
+        mutex.withLock {
+            cache[id] ?: trackDao.getTrackById(id).also {
+                cache.put(id, it)
+            }
+        }
+
+    override suspend fun deleteTrackById(id: Int) {
+        trackDao.deleteTrackById(id)
+        mutex.withLock { cache.remove(id) }
+    }
+
+    companion object {
+        private const val CACHE_SIZE = 1
+    }
+}
